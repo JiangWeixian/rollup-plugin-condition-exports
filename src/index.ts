ad834df5fe1d8ec1f954548be5a1fc253c91a7d8
jiangWeixian <jiangweixian1994@gmail.com>
2021-11-04 23:07:07 +0800

    :recycle:  export single plugin

--- src/index.ts
+++ src/index.ts
@@ -20,117 +20,116 @@ const NAME = 'rollup-plugin-condition-exports'
 
 const log = debug(NAME)
 
-export default function plugin(): Plugin[] {
+export default function plugin(): Plugin {
   let hasTs = false
   let shouldRun = false
   const filter = createFilter([], [/tslib/g])
-  return [
-    rename({
-      include: ['**/*.ts'],
-      map: (name) => {
-        if (name.includes('tslib')) {
-          return name
-        }
-        if (name.endsWith('.js')) {
-          return name.replace('.js', '.es.js')
-        }
-        return name.startsWith('./') ? `${name}.es` : name
-      },
-    }),
-    {
-      name: NAME,
-      options(options) {
-        hasTs =
-          options.plugins?.some((p) => {
-            if (typeof p === 'object') {
-              return p?.name?.includes('typescript')
+  const r = rename({
+    include: ['**/*.ts'],
+    map: (name) => {
+      if (name.includes('tslib')) {
+        return name
+      }
+      if (name.endsWith('.js')) {
+        return name.replace('.js', '.es.js')
+      }
+      return name.startsWith('./') ? `${name}.es` : name
+    },
+  })
+  return {
+    name: NAME,
+    options(options) {
+      hasTs =
+        options.plugins?.some((p) => {
+          if (typeof p === 'object') {
+            return p?.name?.includes('typescript')
+          }
+          return false
+        }) || false
+      return {
+        ...options,
+        external: (id) => id.startsWith('./'),
+      }
+    },
+
+    generateBundle(options, bundle, _isWrite) {
+      const dir = options.dir === '.' ? '' : options.dir || ''
+      shouldRun = !!options.dir
+      if (!shouldRun) {
+        log('Skip because of not in dir mode')
+      }
+      if (options.format === 'cjs') {
+        if (options.dir) {
+          output.main = path.join(dir, 'index.js')
+          Object.keys(bundle).forEach((filename) => {
+            // tslib
+            if (!filter(filename)) {
+              return
             }
-            return false
-          }) || false
-        return {
-          ...options,
-          external: (id) => id.startsWith('./'),
-        }
-      },
-
-      generateBundle(options, bundle, _isWrite) {
-        const dir = options.dir === '.' ? '' : options.dir || ''
-        shouldRun = !!options.dir
-        if (!shouldRun) {
-          log('Skip because of not in dir mode')
-        }
-        if (options.format === 'cjs') {
-          if (options.dir) {
-            output.main = path.join(dir, 'index.js')
-            Object.keys(bundle).forEach((filename) => {
-              // tslib
-              if (!filter(filename)) {
-                return
-              }
-              // types files
-              if (path.extname(filename) === '.ts') {
-                return
-              }
-              setValue(
-                output,
-                `exports/.\\/${strip(filename)}/require`,
-                `./${path.join(dir, filename)}`,
-                {
-                  separator: '/',
-                },
-              )
-            })
-          }
-        }
-        if (options.format === 'es') {
-          if (options.dir) {
-            output.module = path.join(dir, 'index.es.js')
-            Object.keys(bundle).forEach((filename) => {
-              if (!filter(filename)) {
-                return
-              }
-              if (path.extname(filename) === '.ts') {
-                setValue(
-                  output,
-                  `exports/.\\/${strip(filename)}/types`,
-                  `./${path.join(dir, filename)}`,
-                  {
-                    separator: '/',
-                  },
-                )
-                return
-              }
-              setValue(
-                output,
-                `exports/.\\/${strip(filename)}/import`,
-                `./${path.join(dir, filename)}`,
-                {
-                  separator: '/',
-                },
-              )
-            })
-          }
-        }
-        if (hasTs) {
-          output.typings = path.join(dir, 'index.d.ts')
-        }
-        setValue(output, 'exports/.', './package.json', {
-          separator: '/',
-        })
-        log('%O', output)
-      },
-
-      /* Type: (bundle: { [fileName: string]: AssetInfo | ChunkInfo }) => void */
-      /* Kind: async, parallel                                                  */
-      writeBundle() {
-        if (shouldRun) {
-          const pkg = fs.readJSONSync(path.resolve(process.cwd(), 'package.json'))
-          const next = Object.assign(pkg, output)
-          fs.writeJSONSync(path.resolve(process.cwd(), 'package.json'), next, {
-            spaces: 2,
+            // types files
+            if (path.extname(filename) === '.ts') {
+              return
+            }
+            setValue(
+              output,
+              `exports/.\\/${strip(filename)}/require`,
+              `./${path.join(dir, filename)}`,
+              {
+                separator: '/',
+              },
+            )
           })
         }
-      },
+      }
+      if (options.format === 'es') {
+        if (options.dir) {
+          r.generateBundle?.apply(this, [options, bundle, _isWrite])
+          output.module = path.join(dir, 'index.es.js')
+          Object.keys(bundle).forEach((filename) => {
+            if (!filter(filename)) {
+              return
+            }
+            if (path.extname(filename) === '.ts') {
+              setValue(
+                output,
+                `exports/.\\/${strip(filename)}/types`,
+                `./${path.join(dir, filename)}`,
+                {
+                  separator: '/',
+                },
+              )
+              return
+            }
+            setValue(
+              output,
+              `exports/.\\/${strip(filename)}/import`,
+              `./${path.join(dir, filename)}`,
+              {
+                separator: '/',
+              },
+            )
+          })
+        }
+      }
+      if (hasTs) {
+        output.typings = path.join(dir, 'index.d.ts')
+      }
+      setValue(output, 'exports/.', './package.json', {
+        separator: '/',
+      })
+      log('%O', output)
     },
-  ]
+
+    /* Type: (bundle: { [fileName: string]: AssetInfo | ChunkInfo }) => void */
+    /* Kind: async, parallel                                                  */
+    writeBundle() {
+      if (shouldRun) {
+        const pkg = fs.readJSONSync(path.resolve(process.cwd(), 'package.json'))
+        const next = Object.assign(pkg, output)
+        fs.writeJSONSync(path.resolve(process.cwd(), 'package.json'), next, {
+          spaces: 2,
+        })
+      }
+    },
+  }
 }
