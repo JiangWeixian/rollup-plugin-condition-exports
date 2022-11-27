88c5c04a8ddab87ab958d9584bd974d94ec5db1d
JiangWeixian <jiangweixian1994@gmail.com>
2022-11-27 10:58:22 +0800

    :white_check_mark:  search input & resolve pkg

--- test/resolver.test.ts
+++ test/resolver.test.ts
@@ -1,14 +1,37 @@
-import { describe, it } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import path from 'path'
 
 import { PackageContext } from '../src/convention/context'
 
-const FIXTURES = path.resolve(__dirname, '../examples/basic')
+const FIXTURES = path.resolve(__dirname, './fixtures')
 
 describe('resolver', () => {
-  it('resolve exports', async () => {
-    const ctx = new PackageContext({ dirs: 'src/exports' }, FIXTURES)
+  it('search inputs', async () => {
+    const ctx = new PackageContext({ dirs: 'basic' }, FIXTURES)
     await ctx.searchGlob()
-    await ctx.resolveRoutes()
+    const inputs = await ctx.resolveInputs()
+    expect(inputs.map((v) => v.replace(FIXTURES, ''))).toMatchInlineSnapshot(`
+[
+  "/basic/about.tsx",
+  "/basic/components.tsx",
+  "/basic/index.tsx",
+  "/basic/no-condition.tsx",
+  "/basic/about/index.tsx",
+  "/basic/blog/[...all].tsx",
+  "/basic/blog/[id].tsx",
+  "/basic/blog/index.tsx",
+  "/basic/condition/browser.tsx",
+  "/basic/condition/node.tsx",
+  "/basic/no-condition/node.tsx",
+  "/basic/blog/today/[...all].tsx",
+  "/basic/blog/today/index.tsx",
+]
+    `)
+  })
+  it('resolve pkg', async () => {
+    const ctx = new PackageContext({ dirs: 'basic' }, FIXTURES)
+    await ctx.searchGlob()
+    const pkg = await ctx.resolvePkg()
+    expect(pkg).toMatchSnapshot()
   })
 })
